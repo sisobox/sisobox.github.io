bc49c0993e2208790afb3fcfaa1966472412ccee
Lim SungGil <imddola@naver.com>
2015-01-28 16:00:14 +0900

    입력창 IE, Chrome, Safari 호환을 위해 재작업 내용 추가. ie8 전용 style.css 삭제.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,29 +1,15 @@
 var flagKOR = ["대한민국", "미국", "일본", "유럽연합", "중국", "호주", "캐나다", "뉴질랜드"];
 var flagENG = ["krw", "usd", "jpy", "eur", "cny", "aud", "cad", "nzd"];
-var flagPaddingRightPx = [5, 32, 32, 5, 32, 32, 18, 5]; 
 
 $(document).ready(function() {
-	changeFlag(1, 1);
-	changeFlag(2, 0);
 	//http://community.fxkeb.com/fxportal/jsp/RS/DEPLOY_EXRATE/fxrate_all.html
 	//http://fx.keb.co.kr/FER1101C.web?schID=fex&mID=FER1101C
 });	
 
-$('inputText1').on('keydown', function(event) {
-    console.log(event.keyCode);
-});
-
 function changeFlag(input, flag) {
-	var text = "";
-	var text = text + "<table>";
-	var text = text + "<tr><td>";
-	var text = text + "<img src='./images/" + flagENG[flag] + ".png'>";
-	var text = text + "<strong style='padding-left:5px; padding-right:" + flagPaddingRightPx[flag] + "px;'>" + flagKOR[flag] + "</strong>";
-	var text = text + "<span class='caret'>";
-	var text = text + "</td></tr>";
-	var text = text + "<tr><td>" + flagENG[flag].toUpperCase() + "</td></tr>";
-	var text = text + "</table>";
-	$(".selectInfo" + input).html(text);
+	$('#selectFlag' + input).attr("src", "./images/" + flagENG[flag] + ".png");
+	$('#selectBtnText' + input).text(flagKOR[flag]);
+	$('#selectEng' + input).text(flagENG[flag].toUpperCase());
 }
 
 
